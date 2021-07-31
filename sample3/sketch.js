fe600bb9a5b97d76fc7c14f7d1afb99353b1ca67
yusnmt <yusnmt@gmail.com>
2021-07-31 22:11:55 +0900

    create numeric

--- sample3/sketch.js
+++ sample3/sketch.js
@@ -1,7 +1,33 @@
 function setup() {
-  createCanvas(400, 400);
+  createCanvas(windowWidth, windowHeight);
 }
 
 function draw() {
-  background(220);
+  let numA = 10;
+  let numB = 6;
+  let scalar = 50;
+  numA *= scalar;
+  numB *= scalar;
+
+  let wd = numB;
+  let xPos = 0;
+  let yPos = 0;
+  let itr = 0;
+
+  while(wd > 0){
+    itr++;
+    if(itr % 2 == 1){
+      while(xPos + wd <= numA){
+        rect(xPos, yPos, wd, wd);
+        xPos += wd;
+      }
+      wd = numA - xPos;
+    } else {
+      while(yPos + wd <= numB){
+        rect(xPos, yPos, wd, wd);
+        yPos += wd;
+      }
+      wd = numB - yPos
+    }
+  }
 }
